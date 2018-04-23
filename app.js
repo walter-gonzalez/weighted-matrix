515c2f16cd2e5ba49918358ee0410a1938ab523f
Walter González <walter.gonzalez@woow.com.uy>
2018-04-23 11:28:05 -0300

    transforme matrix en un objeto

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 (function() {
 
-   
+var matrix = {
+    description : '<strong>Descripción:</strong> Breve descripción opcional de la matriz y el proyecto.',
+    designsCount : 2,
+    criteriasCount : 1,
+    maxDesigns : 5,
+    maxCriterias : 10,
+    defaultCriteria : 
+        {
+            title : 'Criterio' + this.criteriasCount++,
+            weight : 0
+        },
+    defaultDesign :
+        {
+            title : 'Diseño' + this.designsCount++,
+            ratings : [0],
+            scores : [0],
+            total : 0
+        },
+    criterias : [],
+    designs : [],
+    addCriteria : function() {
+        console.log('add criteria');
+    },
+    addDesign : function() {
+        console.log('add design');
+    },
+    init : function() {
+        console.log('init');
 
+        //Escribo descripcion por defecto...
+        var descriptionCell = document.querySelector('.matrix-description');
+        descriptionCell.innerHTML = this.description;
+
+        //Guardo los botones...
+        var addCriteriaBtn = document.getElementById("add-criteria");
+        var addDesignBtn = document.getElementById("add-design");
+
+        //Listeners para botones...
+        addCriteriaBtn.addEventListener("click", matrix.addCriteria.bind(this));
+        addDesignBtn.addEventListener("click", matrix.addDesign.bind(this));
+
+        this.render();
+    },
+    render : function() {
+        console.log('render');
+    }
 }
-);
+
+matrix.init();
+   
+}
+)();
